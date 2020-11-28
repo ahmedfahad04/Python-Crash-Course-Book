54a1498c4c9752a5a52faef57ebd4ab83e789e7d
Fahad <ahmedfahad3596@gmail.com>
2020-11-28 21:45:50 +0600

    Day 3

--- 6. Dictionaries/Nesting.py
+++ 6. Dictionaries/Nesting.py
@@ -53,7 +53,34 @@ pizza = {
 }
 
 # summarize the order
-print("You ordered a "+ pizza['crust']+"-crust pizza with the following toppings:")
+print("You ordered a " + pizza['crust'] +
+      "-crust pizza with the following toppings:")
 
 for top in pizza['toppings']:
     print("\t" + top)
+
+
+# a dictionary in a dictionary
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+
+for username, user_info in users.items():
+    print("\nUsername: "+ username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: "+ full_name.title())
+    print("\tLocation: "+ location.title())
+
+
