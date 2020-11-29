33f60905e4d0d220807d2020023838ddffe854c2
Fahad <ahmedfahad3596@gmail.com>
2020-11-29 11:16:53 +0600

    Day 4

---
+++ 7. User_Input_and_while_loop/Introduction_to_while_loop.py
@@ -0,0 +1,18 @@
+# loop while in action
+
+current_number = 1
+
+while current_number<=10:
+    print("The number is ", current_number)
+    current_number += 1
+
+# let user choose when to quit
+
+promt = "\nTell me something, and I will repeat it back to you: "
+promt += "\nEnter 'quit' to end the program."
+
+msg = ''
+
+while msg != 'quit':
+    msg = input(promt)
+    print(msg)
