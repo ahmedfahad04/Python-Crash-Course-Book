7ada043cb771c935652bf71aaae715067ab4eab4
Fahad <ahmedfahad3596@gmail.com>
2020-11-30 09:02:00 +0600

    Day 5

---
+++ 8. Function/Passing_arguments.py
@@ -0,0 +1,13 @@
+# POSITIONAL ARGUMENTS
+
+def describe_pet(animal_type, pet_name):
+    print("\nI have a "+ animal_type+".")
+    print("My "+ animal_type+"'s name is "+ pet_name.title()+".")
+
+describe_pet('hamster','harry')
+describe_pet('dog','willie')
+
+# KEYWORD ARGUMENT
+
+describe_pet(animal_type='cat',pet_name='jony')
+
