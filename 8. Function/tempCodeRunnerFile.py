454c781636a9021979de7ed2ce60a8a2e01d66ca
Fahad <ahmedfahad3596@gmail.com>
2020-12-02 10:49:41 +0600

    Day 5

--- 8. Function/tempCodeRunnerFile.py
+++ 8. Function/tempCodeRunnerFile.py
@@ -1,8 +1,6 @@
-def make_pizza(*toppings):
-#     print("\nMaking a pizza with the following toppings")
-#     for topping in toppings: 
-#         print("- "+topping)
+unprinted_designs = ['iphone case','robot pendant','dodecahendron','motorolla']
+# completed_models = []
 
+# pm(unprinted_designs, completed_models)
+# pf.show_completed(completed_models)
 
-# make_pizza("pepperoni")
-# make_pizza('mushroooms','green pepper','extra cheese')
