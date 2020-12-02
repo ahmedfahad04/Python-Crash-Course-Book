7f16af04beb9b4ec62c7592bd6a5f629d421de37
Fahad <ahmedfahad3596@gmail.com>
2020-12-02 08:50:10 +0600

    Day 5

--- 8. Function/Try_it_yourself_passing_list.py
+++ 8. Function/Try_it_yourself_passing_list.py
@@ -1,26 +1,51 @@
 #8.9 magicians
 
-name = ['juel','rana','auch']
-# def show_magicians(names):
-#     for person in names:
-#         print(person.title())
+def show_magician(names):
+    for magician in names:
+        print(magician.title())
 
-# show_magicians(name)
+magicians = ['juel','aich','sohel','rana']
+show_magician(magicians)
 
-print("--------------------------------------------")
+print("--------------------------")
 #8.10 great magicians
 
-def make_great(names):
-    for person in names: 
-        person = "Great "+person.title()
-        print(person)
+def make_great(magics):
+    temp_great = []
 
-make_great(name)
+    #print(magics)
+    while magics:
+        hold = magics.pop()
+        x = "Great " + hold.title()
+        temp_great.append(x)
+    ##print(temp_great)
 
+    for item in temp_great:
+        magics.append(item)
 
-def show_magicians(names):
-    for person in names:
-        print(person.title())
+    #return magics
 
-show_magicians(name)
+magicians = ['juel','aich','sohel','rana']
+make_great(magicians)
+show_magician(magicians)
+    
+print("-----------------------------------------")
+#8.11 unchagned magician
 
+def make_great_2(magics):
+    temp_great = []
+
+    #print(magics)
+    while magics:
+        hold = magics.pop()
+        x = "Great " + hold.title()
+        temp_great.append(x)
+    #print(temp_great)
+    return temp_great
+
+magicians = ['juel','aich','sohel','rana']
+print("\nOriginal List is: ")
+show_magician(magicians)
+
+print("\nModified List is: ")
+print("\n".join(make_great_2(magicians)))
